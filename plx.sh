89103f3c08f2583997fd5fb6a2df651d99fe96d5
rknoesen <rudiknoesen@gmail.com>
2020-06-27 18:52:11 +0200

    lala

--- plx.sh
+++ plx.sh
@@ -1,3 +1,3 @@
 sudo chmod 755 /run/media/rknoesen
-/run/media/rknoesen/BOB2T/Movies
-/run/media/rknoesen/BOB2T/Series
+sudo setfacl -R -m u:plex:rwx /run/media/rknoesen/BOB2T/Movies
+sudo setfacl -R -m u:plex:rwx /run/media/rknoesen/BOB2T/Series
